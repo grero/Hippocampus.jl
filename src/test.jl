e7dfcc2b098d3cc08f1cd4476f4be75d5f215782
Roger Herikstad <roger.herikstad@gmail.com>
2024-07-19 09:35:06 +0800

    Initial commit

---
+++ src/test.jl
@@ -0,0 +1,183 @@
+using GLMakie
+using ContinuousWavelets
+
+
+function get_time_slice(x::AbstractVector{T}, idx) where T
+    x[idx]
+end
+
+function get_observable(x::AbstractVector{T},idx) where T
+    Observable(get_time_slice(x,idx))
+end
+
+function get_observable(X::Matrix{T},idx) where T
+    Observable(get_time_slice(X,idx))
+end
+
+function get_time_slice(X::Matrix{T},idx) where T
+    X[idx,:]
+end
+
+function plot_heatmap(x::Vector{T1}, X::Matrix{T2},M::Integer;t=1:size(X,1),freqs=1:size(X,2),freq_bands::Union{Nothing, Vector{Tuple{Float64,Float64}}}=nothing) where T1 where T2
+    fig = Figure()
+    axes = [Axis(fig[i,1]) for i in 1:3]
+    rowsize!(fig.layout, 1, Relative(0.1))
+    linkxaxes!(axes[2:end]...)
+    n = size(X,1)
+    idx = 1
+    indices = idx:idx+M-1
+    obs = [get_observable(_x, indices) for _x in [x,X]]
+    tt = get_observable(t, indices)
+    tend = Observable([t[idx+M-1]])
+    on(events(fig.scene).keyboardbutton) do event
+        if event.action == Keyboard.press
+            do_update = false
+            if event.key == Keyboard.left
+            if idx > M
+                    idx -= M
+                    do_update = true
+            end
+            elseif event.key == Keyboard.right
+                if idx < n-2*M
+                    idx += M
+                    do_update = true
+                end
+            end
+            if do_update
+                #img[] = X[idx:idx+M-1,:]
+                for (_x,_ob) in zip([x,X], obs)
+                    _ob[] = get_time_slice(_x, idx:idx+M-1) 
+                end
+                tt[] = t[idx:idx+M-1]
+                tend[] = [t[idx+M-1]]
+                for ax in axes[2:end]
+                    autolimits!(ax)
+                end
+            end
+        end
+    end
+    for ax in axes[1:end-1]
+        ax.xticklabelsvisible = false
+    end
+    barplot!(axes[1], [1.0], tend, direction=:x)
+    xlims!(axes[1], t[1], t[end])
+    axes[1].yticklabelsvisible = false
+    axes[1].yticksvisible = false
+    lines!(axes[2], tt, obs[1])
+    heatmap!(axes[3], tt, freqs, obs[2])
+    if freq_bands !== nothing
+        # highlight these bands
+        for fb in freq_bands
+            hlines!(axes[3], fb[1],fb[2],color=:white)
+        end
+    end
+    ax = axes[3]
+    ax.xlabel = "Time"
+    ax.ylabel = "Frequency"
+    fig
+end
+
+function get_spectrum(x::Vector{T},fs::T2;β=2) where T <: Real where T2 <: Real
+    n = length(x)
+    c = wavelet(Morlet(π), averagingType=NoAve(), β=β)
+    daughers, ω = computeWavelets(n,c)
+    freqs = getMeanFreq(daughers,fs)
+    res = cwt(x, c, daughters)
+    res, freqs
+end
+
+function get_reward_aligned_spectrum(;window=(-500,500), β=1.5, maxfreq=100.0)
+    data = MAT.matread("vmlfp.mat")
+    fs = data["vp"]["data"]["analogInfo"]["SampleRate"]
+    dd = data["vp"]["data"]["analogData"][:]
+    # findall rewarded trials
+    ridx = findall(30 .< data["vp"]["data"]["markers"][:,3] .< 40)
+    # reward onsets in units of ms
+    reward_onsets = round.(Int64, ceil.(data["vp"]["data"]["timeStamps"][ridx,3]*1000))
+   
+    #this is probably massively inefficient
+
+    # compute wavelet-bases power-spectrum
+    n = window[2]-window[1]
+    c = wavelet(Morlet(π), averagingType=NoAve(), β=β)
+    daughters, ω = computeWavelets(n,c)
+    freqs = getMeanFreq(daughters,fs) 
+
+    # Average reward aligned power spectrum
+    P = fill(0.0, 1000, length(freqs))
+    Ps = fill!(similar(P), 0.0)
+    P2 = fill(0.0, 1000, length(freqs))
+    for onset in reward_onsets
+        x = dd[onset+window[1]:onset+window[2]-1]
+        res = cwt(x, c, daughters) 
+        res_s = cwt(circshift(x, rand(-n+1:n-1)),c,daughters)
+        ll = log.(abs.(res))
+        ll_s = log.(abs.(res_s))
+        P .+= ll
+        Ps .+= ll_s
+        P2 .+= ll.*ll
+    end
+    P ./= length(reward_onsets)
+    Ps ./= length(reward_onsets)
+    P2 ./= length(reward_onsets)
+    P2 .-= P.*P
+    P2 .= sqrt.(P2)
+    P, Ps, range(window[1], stop=window[2], length=n), freqs
+end
+
+function plot_trial(unity_data::Matrix{T}, lfp_data::Vector{T2}, unity_triggers::Matrix{T}, lfp_triggers::Matrix{T2}) where T <: Real where T2 <: Real
+    ntrials = size(lfp_triggers,1)
+    nu = size(unity_data,1)
+    nl = length(lfp_data)
+    xmin,xmax = extrema(unity_data[:,3])
+    ymin,ymax = extrema(unity_data[:,4])
+    tidx = Observable(1)
+    pos = Observable(Point2f[])
+    lfp = Observable(Point2f[])
+    trial_start_lfp = Observable(0)
+    trial_start_unity = Observable(Point2f[])
+    on(tidx) do _tidx
+        uidx0 = max(round(Int64, unity_triggers[tidx[], 1]), -1, 1)
+        uidx1 = min(round(Int64, unity_triggers[tidx[], 3])+1, nu)
+        pos[] = [Point2f(unity_data[i,3:4]...) for i in uidx0:uidx1]
+        trial_start_unity[] = [Point2f(unity_data[uidx0+1,3], unity_data[uidx0+1,4])]
+
+        idx0 = max(round(Int64, lfp_triggers[tidx[], 1]*1000)-1000,1)
+        idx1 = min(round(Int64, lfp_triggers[tidx[], 3]*1000)+1000,nl)
+        lfp[] = [Point2f.(i, lfp_data[i]) for i in idx0:idx1]
+        trial_start_lfp[] = idx0+1000
+    end
+
+    fig = Figure()
+
+    on(events(fig.scene).keyboardbutton) do event
+        if event.action == Keyboard.press
+            do_upate = false
+            if event.key == Keyboard.left
+                if tidx[] > 0 
+                    tidx[] = tidx[] - 1 
+                    do_update = true
+                end
+            elseif event.key == Keyboard.right
+                if tidx[] < ntrials 
+                    tidx[] = tidx[] + 1 
+                    do_update = true
+                end
+            end
+            if do_update
+                autolimits!(axes[2])
+            end
+        end
+    end
+
+    axes = [Axis(fig[i,1]) for i in 1:2]
+    lines!(axes[1], pos)
+    scatter!(axes[1], trial_start_unity, color=:red)
+    xlims!(axes[1], xmin, xmax)
+    ylims!(axes[1], ymin, ymax)
+
+    lines!(axes[2], lfp)
+    vlines!(axes[2],trial_start_lfp, color=:red)
+    tidx[] = 1
+    fig
+end
