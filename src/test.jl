b1e75bdff5fe7283ad6296a2b4956be9221921c6
Roger Herikstad <roger.herikstad@gmail.com>
2024-08-01 15:55:17 +0800

    Refactor out trial marker reshaping

--- src/test.jl
+++ src/test.jl
@@ -132,27 +132,8 @@ end
 function RippleData(markers::Vector{UInt16}, timestamps::Vector{Float64})
     # ignore markers with value 0
     idx = markers.>0
-    new_markers = Int64.(markers[idx])
-    new_timestamps = timestamps[idx]
-    # the first marker is a session start; the remaining come in trios
-    nn = length(new_markers)
-    rem(nn-1,3) == 0 || error("Inconsistent number of markers")
-    nt = div(nn,3)
-    trial_markers = permutedims(reshape(new_markers[2:end], 3, nt))
-    trial_timestamps = permutedims(reshape(new_timestamps[2:end],3,nt))
-
-    # sanity check; make sure that the last number digit is the same for each trial
-    # and that the succession is 1,2,3 or 1,2,4.
-    main_marker = floor.(trial_markers/10.0)
-    p1 =sum(sum(main_marker .≈ [1.0 2.0 3.0],dims=2).==3)
-    p2 =sum(sum(main_marker .≈ [1.0 2.0 4.0],dims=2).==3)
-
-    p1+p2 == nt || error("Inconsistent main markers")
-
-    # check the the minor markers are the same
-    minor_marker = trial_markers - 10.0*main_marker
-    p3 = sum(sum(minor_marker .== minor_marker[:,1:1],dims=2).==3)
-    p3 == nt || error("Inconsistent cue markers")
+    trial_markers, trial_timestamps = reshape_triggers(Int64.(markers[idx]), timestamps[idx])
+   
     RippleData(trial_markers, trial_timestamps,Dict())
 end
 
