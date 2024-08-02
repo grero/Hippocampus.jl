9f6cac0a2416408332e1a79ad9853e6c1993b3f8
Roger Herikstad <roger.herikstad@gmail.com>
2024-08-02 21:32:55 +0800

    Refactor

--- src/unity.jl
+++ src/unity.jl
@@ -12,13 +12,18 @@ x4Bound = [2.5, 7.5, 7.5, 2.5, 2.5]  # green pillar
 z4Bound = [-2.5, -2.5, -7.5, -7.5, -2.5]
 
 function plot_arena()
-    fig,ax = poly(Point2f.(zip(zBound, xBound)),color=:grey)
-    ax.aspect = 1.0 
+    fig = Figure()
+    ax = Axis(fig[1,1])
+    plot_arena!(ax)
+    fig,ax
+end
+
+function plot_arena!(ax)
+    poly!(ax, Point2f.(zip(zBound, xBound)),color=:grey)
     poly!(ax, Point2f.(zip(z1Bound, x1Bound)), color=:yellow)
     poly!(ax, Point2f.(zip(z2Bound, x2Bound)), color=:red)
     poly!(ax, Point2f.(zip(z3Bound, x3Bound)), color=:blue)
     poly!(ax, Point2f.(zip(z4Bound, x4Bound)), color=:green)
-    fig,ax
 end
 
 function impacts(pos)
