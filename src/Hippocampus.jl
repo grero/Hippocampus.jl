af48cf9eebb10f1ecca00b4dc4064d7668a80cd3
Roger Herikstad <roger.herikstad@gmail.com>
2024-08-26 12:21:04 +0800

    Add missing import

--- src/Hippocampus.jl
+++ src/Hippocampus.jl
@@ -5,6 +5,7 @@ using DrWatson
 using DataProcessingHierarchyTools
 const DPHT = DataProcessingHierarchyTools
 include("utils.jl")
+include("sprites.jl")
 include("eyelink.jl")
 include("rawdata.jl")
 include("unity.jl")
