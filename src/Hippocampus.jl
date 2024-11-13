9025053abbcdd95664c7adf5d5da0bfd2b1d3b65
Roger Heirkstad <roger.herikstad@gmail.com>
2024-11-13 16:14:29 +0800

    Fix import order

--- src/Hippocampus.jl
+++ src/Hippocampus.jl
@@ -7,9 +7,10 @@ const DPHT = DataProcessingHierarchyTools
 include("utils.jl")
 include("eyelink.jl")
 include("rawdata.jl")
-include("unity.jl")
-include("test.jl")
+include("spikedata.jl")
 include("sprites.jl")
+include("unity.jl")
+include("spatial.jl")
 include("replay.jl")
 include("mountainsort.jl")
 end
