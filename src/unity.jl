b0ea65af3d3e257a8dbac2560b0eac752020f39c
Roger Heirkstad <roger.herikstad@gmail.com>
2024-11-28 12:06:01 +0800

    Cosmetic change

--- src/unity.jl
+++ src/unity.jl
@@ -5,14 +5,19 @@ using LinearAlgebra
 using DelimitedFiles
 using FileIO
 
+# TODO: Unclear if these are the latest values. Perhaps update?
 xBound = [-12.5, 12.5, 12.5, -12.5, -12.5]
 zBound = [12.5, 12.5, -12.5, -12.5, 12.5]
+
 x1Bound = [-7.5, -2.5, -2.5, -7.5, -7.5]  # yellow pillar
 z1Bound = [7.5, 7.5, 2.5, 2.5, 7.5]
+
 x2Bound = [2.5, 7.5, 7.5, 2.5, 2.5]  # red pillar
 z2Bound = [7.5, 7.5, 2.5, 2.5, 7.5]
+
 x3Bound = [-7.5, -2.5, -2.5, -7.5, -7.5]  # blue pillar
 z3Bound = [-2.5, -2.5, -7.5, -7.5, -2.5]
+
 x4Bound = [2.5, 7.5, 7.5, 2.5, 2.5]  # green pillar
 z4Bound = [-2.5, -2.5, -7.5, -7.5, -2.5]
 
