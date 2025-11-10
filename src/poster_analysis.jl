362c12b674d1f5a384f56b625014a8516f8f6945
Roger Herikstad <roger.herikstad@gmail.com>
2025-11-10 12:40:00 +0800

    Reduce the time window for the rasters

--- src/poster_analysis.jl
+++ src/poster_analysis.jl
@@ -211,7 +211,7 @@ end
 TODO: [ ] Find less sparse cell examples
       [ ] Show histogram of coding contribution as well
 """
-function plot_poster_decoding_results(;cell_examples=(poster_selective=71, previous_poster_selective=121, both=9),show_psth=false)
+function plot_poster_decoding_results(;cell_examples=(poster_selective=71, previous_poster_selective=121, both=9),show_psth=false,tmin=-1.0, tmax=20.0)
     allcelldirs = open("/Volumes/Hippocampus/Data/picasso-misc/AnalysisHM/Current Analysis/cell_list.txt") do fid
         readlines(fid)
     end
@@ -236,11 +236,11 @@ function plot_poster_decoding_results(;cell_examples=(poster_selective=71, previ
 
         lg21 = GridLayout(lg2[1,1])
         Label(lg21[1,1,TopLeft()], "A")
-        plot_raster_and_psth!(lg21, allcelldirs[cell_examples.previous_poster_selective];previous=true,xlabelvisible=true, xticklabelsvisible=true,binsize=0.1,show_psth=show_psth, show_outliers=false)
+        plot_raster_and_psth!(lg21, allcelldirs[cell_examples.previous_poster_selective];previous=true,xlabelvisible=true, xticklabelsvisible=true,binsize=0.1,show_psth=show_psth, show_outliers=false,tmin=tmin, tmax=tmax)
         rowsize!(lg21, 1, Relative(0.6))
         lg31 = GridLayout(lg2[2,1])
         rowsize!(lg31, 1, Relative(0.6))
-        plot_raster_and_psth!(lg31, allcelldirs[cell_examples.previous_poster_selective];previous=false, binsize=0.1, show_psth=show_psth, show_outliers=false)
+        plot_raster_and_psth!(lg31, allcelldirs[cell_examples.previous_poster_selective];previous=false, binsize=0.1, show_psth=show_psth, show_outliers=false,tmin=tmin,tmax=tmax)
 
         lg22 = GridLayout(lg2[1,2])
         Label(lg22[1,1,TopLeft()], "B")
@@ -252,11 +252,11 @@ function plot_poster_decoding_results(;cell_examples=(poster_selective=71, previ
         rowsize!(lg23, 1, Relative(0.6))
         lg33 = GridLayout(lg2[2,3])
         rowsize!(lg33, 1, Relative(0.6))
-        plot_raster_and_psth!(lg22, allcelldirs[cell_examples.poster_selective];previous=true,ylabelvisible=false,xlabelvisible=true, xticklabelsvisible=true,binsize=0.1, show_psth=show_psth, show_outliers=false)
-        plot_raster_and_psth!(lg32, allcelldirs[cell_examples.poster_selective];previous=false, ylabelvisible=false, binsize=0.1, show_psth=show_psth, show_outliers=false)
+        plot_raster_and_psth!(lg22, allcelldirs[cell_examples.poster_selective];previous=true,ylabelvisible=false,xlabelvisible=true, xticklabelsvisible=true,binsize=0.1, show_psth=show_psth, show_outliers=false,tmin=tmin, tmax=tmax)
+        plot_raster_and_psth!(lg32, allcelldirs[cell_examples.poster_selective];previous=false, ylabelvisible=false, binsize=0.1, show_psth=show_psth, show_outliers=false, tmin=tmin, tmax=tmax)
 
-        plot_raster_and_psth!(lg23, allcelldirs[cell_examples.both];previous=true, ylabelvisible=false, xticklabelsvisible=true, xlabelvisible=true,binsize=0.1, show_psth=show_psth, show_outliers=false)
-        plot_raster_and_psth!(lg33, allcelldirs[cell_examples.both];previous=false, ylabelvisible=false, binsize=0.1,show_psth=show_psth,show_outliers=false)
+        plot_raster_and_psth!(lg23, allcelldirs[cell_examples.both];previous=true, ylabelvisible=false, xticklabelsvisible=true, xlabelvisible=true,binsize=0.1, show_psth=show_psth, show_outliers=false, tmin=tmin, tmax=tmax)
+        plot_raster_and_psth!(lg33, allcelldirs[cell_examples.both];previous=false, ylabelvisible=false, binsize=0.1,show_psth=show_psth,show_outliers=false, tmin=tmin, tmax=tmax)
         # tuning strength of individual cell vs contribution to decoder
         # plot performance on current poster vs previous poster
         lg1 = GridLayout(fig[2,1])
