d6c9c2448c945c8e73f8b5f1515a8addefd82eda
Roger Herikstad <roger.herikstad@gmail.com>
2025-12-08 13:36:47 +0800

    Fix error message

--- src/replay.jl
+++ src/replay.jl
@@ -450,7 +450,7 @@ function UnityRaytraceData(;do_save=true, redo=false,append_tag=true, raytrace_f
             raytrace_fname = joinpath(extradir, raytrace_fname)
         end
         if !ispath(raytrace_fname)
-            error("No raytracing data found in $(pwd())")
+            error("$(raytrace_fname) not found in $(pwd())")
         end
         unity_eyelinkfile = CSV.File(raytrace_fname, header=0)
         n = length(unity_eyelinkfile)
