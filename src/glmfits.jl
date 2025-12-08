c8120be3316a777bb1b3e17c28e1ee30774d093d
Roger Herikstad <roger.herikstad@gmail.com>
2025-12-08 08:05:41 +0800

    Set convergence to true always

--- src/glmfits.jl
+++ src/glmfits.jl
@@ -711,7 +711,7 @@ function GLMFitH(dims::NTuple{N,Symbol}, jocc::JointOccupancy, unity_gaze_data::
             β[:,:,i] = dq[_α][:β]
             ll[:,i] = dq[_α][:ll]
         end
-        glmfit = GLMFitH(β, ll, α, trainidx, nspikes, dims, qidx)
+        glmfit = GLMFitH(β, ll, α, trainidx, nspikes, dims, qidx,true)
         if do_save
             save_jld2(glmfit, fname)
         end
