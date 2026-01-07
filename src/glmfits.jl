bf1a17e3971dc2148d9d456b0a3e7e2fa7e9909c
Roger Herikstad <roger.herikstad@gmail.com>
2026-01-07 16:21:48 +0800

    Fix an issue when loading single variable fits to compute multi-variable fits

--- src/glmfits.jl
+++ src/glmfits.jl
@@ -1300,7 +1300,8 @@ function GLMFitH(dims::NTuple{N,Symbol}, jocc::JointOccupancy, unity_gaze_data::
             use_α = zeros(length(dims))
             validate_α = false
             for (i,(d,rf)) in enumerate(zip(dims,nrefinements))
-                _glmfit = GLMFitH((d,), jocc, unity_gaze_data;α=α,nruns=nruns,nrefinements=[rf],trainidx=trainidx, kwargs...)
+                # we are only doign this to get the \alpha  values
+                _glmfit = GLMFitH((d,), jocc, unity_gaze_data;α=α,nrefinements=[rf],kwargs...)
                 use_α[i],aidx = get_best_α(_glmfit)
             end
         else
