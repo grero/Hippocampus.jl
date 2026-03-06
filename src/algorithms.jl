16d222dcd615140d0f878a0bc12bb6c7f4657a7c
Roger Herikstad <roger.herikstad@gmail.com>
2026-03-06 10:18:13 +0800

    Look for unity raytrace data in the session directory

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -201,7 +201,7 @@ function compute_skaggs_sic(::Type{T}, nshuffles::Integer;nrefinements=(p=3,g=2)
         rp = cd(DPHT.process_level(level(RippleData))) do
             RippleData()
         end
-        unity_gaze_data = cd(DPHT.process_level(level(UnityRaytraceData))) do
+        unity_gaze_data = cd(DPHT.process_level("session")) do
             UnityRaytraceData(raytrace_fname="unityfile_eyelink_new.csv";redo=false)
         end
         jocc = cd(DPHT.process_level(level(JointOccupancy))) do
