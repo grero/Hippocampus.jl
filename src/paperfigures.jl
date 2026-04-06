5f50d3ce8f70f502295887f164754b7ed98059ef
Roger Herikstad <roger.herikstad@gmail.com>
2026-04-06 08:36:39 +0800

    Change to figure 2 plot

--- src/paperfigures.jl
+++ src/paperfigures.jl
@@ -34,6 +34,70 @@ function figure2(spatial_cells::Vector{String},example_idx::Vector{Int64})
     plot_field_summary(Hippocampus.SpatialResponseFields, spatial_cells, example_idx)
 end
 
+function plot_response_field_with_sic(celldir::String,args...)
+    with_theme(plot_theme) do
+        fig = Figure(size=(600,600))
+        lg = GridLayout(fig[1,1])
+        plot_response_field_with_sic!(lg, celldir,args...)
+        fig
+    end
+end
+
+function plot_response_field_with_sic!(lg, celldir::String, ::Type{T}) where T <: Hippocampus.AbstractResponseFields
+    kwargs = (min_speed=1.0, trial_start=2, min_place_obs=5, min_place_duration=0.05, min_view_obs=5, min_view_duration=0.02, pv_threshold=0.001)
+    mm = Hippocampus.get_mesh(T,(p=3,g=2))
+    sic,rfs = cd(celldir) do
+        sic = Hippocampus.compute_skaggs_sic(Hippocampus.get_sic_type(T), 10_000;load_only=true, smooth=true, smoothing_method=:laplace, α=0.1, niter=50,kwargs...)
+        rfs = Hippocampus.get_response_fields(T, 10_000;smooth=true, smoothing_method=:laplace, α=0.1, niter=50,pv_threshold=0.001,kwargs...) 
+        sic,rfs
+    end
+    if isempty(rfs.binidx) || sic === nothing
+        @show jj
+    end
+    lg12 = GridLayout(lg[1,1])
+    Z = rfs.λ
+    if embeddim(mm) == 2
+        ax = Axis(lg12[1,1],aspect=1)
+        hidedecorations!(ax)
+        ax.leftspinevisible = false
+        ax.bottomspinevisible = false
+        # show the outline of the maze
+        viz!(ax, mm;color=:lightgray)
+        viz!(ax,mm;color=Z)
+    else
+        ax = LScene(lg12[1,1],show_axis=false)
+        #TODO: only hide the ceiling if this cell has no field on the ceiling
+        Hippocampus.plotmesh!(ax, mm;color=Z,indicate_north=true,hide_ceiling=true)
+    end
+
+    clusters = Hippocampus.merge_fields(rfs)
+    nclusters = Hippocampus.get_num_fields(rfs)
+    # relative number of times we get cluster of at least length.(clusters) randomly
+    threshold = dropdims(sum(nclusters,dims=2),dims=2)/size(nclusters,2)
+    # only keep fields where the probabilty of getting the same field in the surroages is less than 0.01
+    valid_cluster_idx = findall(threshold .< kwargs.pv_threshold)
+    for cluster in clusters[valid_cluster_idx] 
+        bb = Hippocampus.find_boundary(mm, rfs.binidx[cluster])
+        viz!(ax, bb;color=:black)
+    end
+    Colorbar(lg12[2,1], colorrange=extrema(filter(isfinite, Z)), label="Firing rate [Hz]",vertical=false,tellwidth=false)
+    #colsize!(lg1, 1, Relative(0.6))
+
+    # show SIC distribution
+    #lg2 = GridLayout(fig[1,2])
+    ax2 = Axis(lg12[3,1])
+
+    boxplot!(ax2, fill(1.0, length(sic.sic)), sic.sic,color=:gray,show_notch=true,orientation=:horizontal,show_outliers=false)
+    vlines!(ax2, sic.sic0, linestyle=:dot, color=:black)
+    ax2.yticklabelsvisible = false
+    ax2.yticksvisible = false
+    ax2.bottomspinevisible = true 
+    ax2.leftspinevisible = false
+    ax2.rightspinevisible = false 
+    ax2.xlabel = "SIC"
+    rowsize!(lg12, 1, Relative(0.8))
+end
+
 function plot_field_summary(::Type{T}, celldirs::Vector{String},example_idx::Vector{Int64}) where T<:Hippocampus.AbstractResponseFields
     kwargs = (min_speed=1.0, trial_start=1, min_place_obs=-1, min_place_duration=-1.0, min_view_obs=-1, min_view_duration=-1.0, pv_threshold=0.01)
     mm = Hippocampus.get_mesh(T,(p=3,g=2))
