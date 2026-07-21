c0bf5a273af1c71f01fdec5f26373eab35fb0061
Roger Herikstad <rherikstad@proton.me>
2026-07-21 21:59:07 +0800

    Return ax when plotting flat maze with posters

--- src/unity.jl
+++ src/unity.jl
@@ -1681,5 +1681,6 @@ function plot_flat_maze_with_posters!(lg;start_point::Union{Nothing, Point2f}=no
         if indicate_north
             arrows!(ax, Point2f(0.0, 10.0), Point2f(0.0, 2.0), color=:black,arrowsize=10.0)
         end
+        ax
     end
 end
