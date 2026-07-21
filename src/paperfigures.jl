d56244fbbfc48b65f7ec899be3e8989138a36819
Roger Herikstad <rherikstad@proton.me>
2026-07-21 22:05:56 +0800

    Tweak plot_field_summary figure

--- src/paperfigures.jl
+++ src/paperfigures.jl
@@ -144,12 +144,12 @@ function plot_field_summary(::Type{T}, celldirs::Vector{String},example_idx::Vec
             ax2.leftspinevisible = false
             ax2.rightspinevisible = false 
             ax2.xlabel = "SIC"
-            rowsize!(lg12, 1, Relative(0.8))
+            rowsize!(lg12, 1, Relative(0.9))
         end
 
         # summary showing total number of fields and coverage
         lg3 = GridLayout(fig[2,1]) 
-        Hippocampus.plot_n_fields!(lg3, T,celldirs;labels=["C","D","E","F"], pv_threshold=0.001, smooth=true, smoothing_method=:laplace, α=0.1,niter=50,kwargs...)
+        Hippocampus.plot_n_fields!(lg3, T,celldirs;labels=["C","D","E","F"], pv_threshold=0.001, smooth=true, smoothing_method=:laplace, α=0.1,niter=50,colormap=:jet,floor_offset=-20, kwargs...)
         rowsize!(fig.layout, 1, Relative(0.6))
         fig
     end
