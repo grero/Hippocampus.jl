6538d962dd72e1aaf138457d1ce1a6a89c55efef
Roger Herikstad <roger.herikstad@gmail.com>
2026-08-24 15:29:00 +0800

    Add function for processing multi-session data

--- src/datautils.jl
+++ src/datautils.jl
@@ -25,3 +25,32 @@ function (lc::LevelCounter)(fname::String,cwd=pwd())
     d = lc.idx-idx
     return d <= lc.depth 
 end
+
+function process_multi_session_cell(celldir::String)
+    if occursin("session", celldir)
+        return celldir # this is not a multi-session
+    end
+    daydir = DPHT.get_level_path("day", celldir)
+    unity_eyelinkfile = joinpath(daydir,"session01","unityfile_eyelink_new.csv")
+    target_link = joinpath(daydir,"unityfile_eyelink_new.csv")
+    if isfile(unity_eyelinkfile) && !islink(target_link)
+        symlink(unity_eyelinkfile, target_link)
+    end
+    # This is a bit weird
+    dayname = DPHT.get_level_name("session", celldir)
+    celldirs = glob(replace(celldir, dayname=>joinpath(dayname, "session??")))
+    combined_dir = joinpath(daydir, "session_combined")
+    if !isdir(combined_dir)
+        mkdir(combined_dir)
+    end
+    for _celldir in celldirs
+        sn = DPHT.get_level_name("session", _celldir)
+        targetlink = replace(_celldir, sn=>"session_combined")
+        if !isdir(targetlink)
+            mkpath(targetlink)
+        end
+    end
+    new_celldir = replace(celldir, dayname=>joinpath(dayname, "session_combined"))
+    @assert isdir(new_celldir)
+    new_celldir
+end
