656c85faed949602377ee82cfd0512bc2fd0b775
Roger Herikstad <rherikstad@proton.me>
2026-09-02 08:29:17 +0800

    Display file name used when plotting field statistics

--- src/response_fields.jl
+++ src/response_fields.jl
@@ -647,6 +647,7 @@ function plot_n_fields!(lg, ::Type{T}, celldirs::Vector{String};labels=["A","B",
     end
     hs = string(h, base=16)
     fname = joinpath(@__DIR__, "..","data","field_stats_$(hs).jld2")
+    @show fname
     if !redo && isfile(fname)
         Z,nfields,field_size, field_boundaries, peak_firing_rate, args = JLD2.load(fname, "Z", "nfields","field_size", "field_boundaries","peak_firing_rate", "args")
     else
