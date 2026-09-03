3cc0cc5e86b27ad2542e4159da945be2ec423627
Roger Herikstad <rherikstad@proton.me>
2026-09-03 12:24:44 +0800

    Tweaks to plot function for goal poster tuning

--- src/goal_poster_analysis.jl
+++ src/goal_poster_analysis.jl
@@ -189,16 +189,15 @@ function plot_goal_poster_selectivity(gs::GoalPosterSelectivity)
     μ_nongoal = [mean(λ_nongoal[fidx_nongoal[i],i]) for i in 1:size(gs.w_goal,2)]
 
     Δ, Δs = generate_surrogates(gs)
-
+    width = 200*sum(isfinite.(μ_goal)) + 50
     with_theme(theme_minimal()) do
-        fig = Figure(size=(250,300))
+        fig = Figure(size=(width,300))
         ax0 = Axis(fig[1,1])
         Label(fig[1,1,TopLeft()], "A")
         # For each view view, plot the goal and no-goal firing rate next to each other
         ddp = vec(permutedims([fill(1, length(μ_goal)) fill(2, length(μ_nongoal))]))
         yyp = vec(permutedims([μ_goal μ_nongoal]))
         xxp = vec(permutedims([1:length(μ_goal) 1:length(μ_nongoal)]))
-        @show xxp ddp
         xt = vec(permutedims([1 2] .+ 2*([1:length(μ_goal);] .-1)))
         barplot!(ax0, xt, yyp)
         ax0.xticks = (xt, repeat(["Goal", "No goal"], length(μ_goal)))
@@ -209,10 +208,10 @@ function plot_goal_poster_selectivity(gs::GoalPosterSelectivity)
 
         ax = Axis(fig[1,2])
         Label(fig[1,2, TopLeft()], "B")
-        yy = vec(Δs)
-        xx = reduce(vcat, [fill(i,size(Δs,1)) for i in 1:size(Δs,2)])
+        yy = vec(Δs[:,isfinite.(μ_goal)])
+        xx = reduce(vcat, [fill(i,size(Δs,1)) for i in findall(isfinite.(μ_goal))])
         boxplot!(ax, xx, yy;show_outliers=false, show_notch=true)
-        scatter!(ax, [1:length(Δ);], Δ;color=:orange)
+        scatter!(ax, [1:sum(isfinite.(μ_goal));], Δ[isfinite.(μ_goal)];color=:orange)
         ax.xticklabelsvisible = false
         ax.xticksvisible = false
         ax.ylabel = "Rate(goal) - Rate(no-goal)"
